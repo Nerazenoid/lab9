cb6bcd917b8823baf5a42bd4caea33aa2d54a0f7
Nerazenoid <72524203+Nerazenoid@users.noreply.github.com>
2021-12-14 02:42:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+    </head>
+    <body>
+    <?php
+    if(isset($_POST['button'])){
+        $x = $_POST["x"];
+        $iter = $_POST["iter"];
+        $k = 1;
+        $res = 0;
+        for ($i=0;$i<$iter;$i++){
+			if($i%2==0){
+				$res-=1/($k*(pow($x,$k)));
+			}
+			else{
+				$res+=1/($k*(pow($x,$k)));
+			}
+			$k+=2;		}
+            $res-=(3.14/2);
+            echo ("Ответ: " . $res);
+    }
+        ?>
+    <form method = "POST">
+	<p>Введите значение x меньше 1</p>
+	<p><input type ="text" name="x" value="-1"></p>
+	<p>Введите количество итераций</p>
+	<p><input type="text" name="iter" value="5"></p>
+	<p><input type="submit" name="button"></p>
+</form>
+    </body>
+</html>
